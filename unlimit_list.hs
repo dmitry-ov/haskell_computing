2b50baca52c4ebf2334cee67192c544120d02c75
Dmitry Ovcharenko <d.ovcharenko@energocomm.ru>
2014-12-08 14:21:41 +0300

    make list

--- unlimit_list.hs
+++ unlimit_list.hs
@@ -6,11 +6,45 @@
 
 --Сооруди такой бесконечный список чтобы любой конечный элемент в нем можно было найти.
 
-allPairs = [(x,y) | x <- [0..], y <- [0..]]
+-- allPairs = [(x,y) | x <- [0..], y <- [0..]]
 
-takeWhile (fst.first (/=1)) allPairs
+-- takeWhile (fst.first (/=1)) allPairs
 
 
 --  takeWhile (\x -> fst (x) + snd (x)  < 2 ) allPairs
 --
 --   takeWhile  (\x -> snd(x) < 1 ) allPairs   ==  [(0,0)]
+--
+--
+--
+
+--  перебор списка натуральных чисел
+--      комбинации пар чисел
+--          добавление пар в список, отбрасывание не уникальных значений
+
+-- nats 0 == [0]
+-- nats 1 == [1,0]
+-- nats 2 == [2,1,0]
+
+nats_list :: Int -> [Int]
+nats_list 0 = [0]
+nats_list n = n : nats_list (pred n)
+
+combination :: [Int] -> [(Int,Int)]
+combination xs = do 
+    x <- xs
+    y <- xs
+    [(x,y)]
+
+
+make_list n = combination (nats_list n)
+
+
+
+
+
+
+
+
+
+
