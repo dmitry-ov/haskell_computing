6363105fcc1bbc6da976a7bd34ce833bb8f5ffb0
Dmitry Ovcharenko <d.ovcharenko@energocomm.ru>
2015-06-02 18:41:28 +0300

    part Case expressions

---
+++ learn.hs
@@ -0,0 +1,47 @@
+head'::[a]->a
+head'[] = error "Error. List is empty!"
+head'(x:_) = x
+
+tell :: (Show a) => [a] -> String  
+tell [] = "The list is empty"  
+tell (x:[]) = "The list has one element: " ++ show x  
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y  
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y  
+
+max' :: (Ord a) => a -> a -> a  
+max' a b | a > b = a | otherwise = b  
+
+myCompare :: (Ord a) => a -> a -> Ordering  
+a `myCompare` b  
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT  
+
+capital :: String -> String  
+capital "" = "Empty string, whoops!"  
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]  
+
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          skinny = 18.5  
+          normal = 25.0  
+          fat = 30.0  
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2] 
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis xs = [bmi w h | (w, h) <- xs]  
+    where bmi weight height = weight / height ^ 2  
+
+initials :: String -> String -> String  
+initials (f:_) (l:_) = [f] ++ "." ++ [l] ++ "."  
+
+
+
+
